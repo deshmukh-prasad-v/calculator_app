3c197f71310f6331b385b8b86764917e08590135
deshmukh-prasad-v <deshmukh.prasad.v@gmail.com>
2025-08-04 14:28:49 +0530

    commit1

--- src/main/java/com/hsbc/simple_app/App.java
+++ src/main/java/com/hsbc/simple_app/App.java
@@ -1,11 +1,29 @@
 package com.hsbc.simple_app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
     public static void main( String[] args ){
-        System.out.println(new App().greet("Prasad"));
+    	App app=new App();
+    	BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
+    	try {
+    		System.out.println("Enter numbers");
+    		int a=Integer.parseInt(bufferedReader.readLine());
+    		int b=Integer.parseInt(bufferedReader.readLine());
+    		System.out.printf("sum: %s",app.add(a, b));
+    		System.out.printf("difference: %s",app.subtract(a, b));
+    	}catch(IOException e) {
+    		System.out.println(e);
+    	}
     }
     
-    public String greet(String name) {
-    	return String.format("Hello %s",name);
+    public double add(double a, double b) {
+    	return a+b;
+    }
+    
+    public double subtract(double a, double b) {
+    	return a-b;
     }
 }
--- src/test/java/com/hsbc/simple_app/AppTest.java
+++ src/test/java/com/hsbc/simple_app/AppTest.java
@@ -6,8 +6,18 @@ import org.junit.jupiter.api.Test;
 
 public class AppTest {
 	@Test
-	public void testGreet() {
+	public void testAdd() {
 		App app=new App();
-		assertEquals(app.greet("Prasad"), "Hello Prasad");
+		assertEquals(100,app.add(50, 50));
+		assertEquals(100,app.add(60, 40));
+		assertEquals(100,app.add(30, 70));
+		assertEquals(100,app.add(-100, 200));
+		assertEquals(100,app.subtract(150, 250));
+	}
+	
+	@Test
+	public void testSubtract() {
+		App app=new App();
+		assertEquals(100,app.subtract(150, 250));
 	}
 }
